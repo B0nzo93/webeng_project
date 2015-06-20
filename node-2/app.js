4c0eed6f5b9236afb4325f947c6a9dbfb021f626
Christoph Besel <christoph.besel@googlemail.com>
2015-06-21 01:37:00 +0200

    added other restful functionality for adding and editing notes and categories, improved deleting

--- node-2/app.js
+++ node-2/app.js
@@ -9,6 +9,9 @@ pool.on('error', function(err) {
 // Define routes
 var express = require("express");
 var app     = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json({type: "*/*"}));
+
 
 app.use(express.static("static"));
 app.get('/notes', function (req, res) {
@@ -17,11 +20,20 @@ app.get('/notes', function (req, res) {
 app.delete('/notes/:id', function (req, res) {
 	deleteNote(req, res);
 });
-app.delete('/category/:id', function (req, res) {
+app.delete('/categories/:id', function (req, res) {
 	deleteCategory(req, res);
 });
-app.post('/notes/', function (req, res) {
+app.put('/notes/', function (req, res) {
 	createNote(req, res);
+});
+app.put('/categories/:name', function (req, res) {
+	createCategory(req, res);
+});
+app.post('/categories/:name', function (req, res) {
+	updateCategory(req, res);
+});
+app.post('/notes/', function (req, res) {
+	updateNote(req, res);
 });
 
 function deleteNote(req, res) {
@@ -32,8 +44,8 @@ function deleteNote(req, res) {
 		}
 		//errorCallback(err);
 	} else {
-		var id = req.params.id
-		var sql = "DELETE FROM todo WHERE id="+id
+		var id = req.params.id;
+		var sql = "DELETE FROM todo WHERE id="+id;
 		con.query(sql, function(err, rows) {
 			con.release();
 			if (err) {
@@ -78,10 +90,43 @@ function createNote(req, res) {
 		}
 		//errorCallback(err);
 	} else {
-		var sql = ""
+		var title = req.body.title;
+		var description = req.body.description;
+		var created = req.body.created;
+		var done = req.body.done;
+		var category_id = req.body.category_id;
+
+		var sql = "INSERT INTO todo (id, description, title, created, done, category_id) " 
+					+ 'VALUES (NULL,"' + description + '","' + title + '","' + created +  '",' + done + ',' + category_id + ');';
+		console.log(sql);
 		con.query(sql, function(err, rows) {
 			con.release();
 			if (err) {
+				console.log(err);
+				//errorCallback(err);
+			} else {
+				//successCallback(rows);
+				res.json(rows);
+			}
+		});
+	}
+	});
+}
+
+function createCategory(req, res) {
+	pool.getConnection(function(err, con) {
+	if (err) {
+		if(con) {
+			con.release();
+		}
+		//errorCallback(err);
+	} else {
+		var name = req.params.name;
+		var sql = 'INSERT INTO category (id, name) VALUES (NULL,"' + String(name) + '");';
+		con.query(sql, function(err, rows) {
+			con.release();
+			if (err) {
+				console.log(err);
 				//errorCallback(err);
 			} else {
 				//successCallback(rows);
@@ -104,6 +149,67 @@ function selectAllNotes(req, res) {
 		con.query("SELECT t.id, t.title, t.description, t.created, t.done, c.id as category_id, c.name as category_name FROM todo t JOIN category c ON t.category_id = c.id", function(err, rows) {
 			con.release();
 			if (err) {
+				//errorCallback(err);
+			} else {
+				//successCallback(rows);
+				res.json(rows);
+			}
+		});
+	}
+	});
+}
+
+function updateCategory(req, res) {
+	pool.getConnection(function(err, con) {
+	if (err) {
+		if(con) {
+			con.release();
+		}
+		//errorCallback(err);
+	} else {
+		var id = req.body.id;
+		var new_name = req.body.name;
+
+		var sql = 'UPDATE category SET name="' + String(new_name) + '" WHERE id=' + id;
+		con.query(sql, function(err, rows) {
+			con.release();
+			if (err) {
+				console.log(err);
+				//errorCallback(err);
+			} else {
+				//successCallback(rows);
+				res.json(rows);
+			}
+		});
+	}
+	});
+}
+
+function updateNote(req, res) {
+	pool.getConnection(function(err, con) {
+	if (err) {
+		if(con) {
+			con.release();
+		}
+		//errorCallback(err);
+	} else {
+		var id = req.body.id;
+		var title = req.body.title;
+		var description = req.body.description;
+		var created = req.body.created;
+		var done = req.body.done;
+		var category_id = req.body.category_id;
+
+		var sql = 'UPDATE todo SET title="' + title + '", ' +
+										'description="'+ description + '", ' +
+										'created="' + created +'", ' +
+										'done=' + done + ', ' +
+										'category_id=' + category_id + ' WHERE id=' + id;
+		console.log(sql);
+		con.query(sql, function(err, rows) {
+			con.release();
+			if (err) {
+				console.log(err);
 				//errorCallback(err);
 			} else {
 				//successCallback(rows);
