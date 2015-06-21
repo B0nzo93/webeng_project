bac3071ebdc4bfd48b4efbcd9bfbf06f0da4cf3d
Marco Ziegaus <git@marcoziegaus.de>
2015-06-21 16:42:39 +0200

    Implemented universal errorhandling

--- node-2/controllers/category.js
+++ node-2/controllers/category.js
@@ -1,6 +1,19 @@
 module.exports = {};
 
 
+function errorhandler(res, con) {
+	return function(err, rows) {
+		con.release();
+		if (err) {
+			console.error(err);
+			res.status(400).send("Error on SQL execution.");
+		} else {
+			res.status(200).json(rows);
+		}
+	}
+}
+
+
 /**
 * This function selects all categories in the databse extracted from a request.
 *
@@ -9,28 +22,19 @@ module.exports = {};
 */
 module.exports.selectAll = function selectAllCategories(req, res) {
 	module.exports.mysql.getConnection(function(err, con) {
-	if (err) {
-		if(con) {
-			con.release();
-		}
-		console.error(err);
-		res.sendStatus(500, "Database error");
-	} else {
-		var query = module.exports.squel.select()
-										.from("category");
-		con.query(query.toString(), function(err, rows) {
-			con.release();
-			if (err) {
-				console.error(err);
-				res.sendStatus(404);
-			} else {
-				res.status(200).json(rows);
+		if (err) {
+			if(con) {
+				con.release();
 			}
-		});
-	}
+			console.error(err);
+			res.status(500).send("Database error.");
+		} else {
+			var query = module.exports.squel.select()
+											.from("category");
+			con.query(query.toString(), errorhandler(res, con));
+		}
 	});
 };
-
 
 /**
 * This function deletes a category in the database extracted from a request.
@@ -40,27 +44,19 @@ module.exports.selectAll = function selectAllCategories(req, res) {
 */
 module.exports.delete = function deleteCategory(req, res) {
 	module.exports.mysql.getConnection(function(err, con) {
-	if (err) {
-		if(con) {
-			con.release();
-		}
-		console.error(err);
-		res.sendStatus(500, "Database error");
-	} else {
-		var id = req.params.id;
-		var query = module.exports.squel.delete()
-					.from("category")
-					.where("id=?", id);
-		con.query(query.toString(), function(err, rows) {
-			con.release();
-			if (err) {
-				console.error(err);
-				res.sendStatus(404);
-			} else {
-				res.status(200).json(rows);
+		if (err) {
+			if(con) {
+				con.release();
 			}
-		});
-	}
+			console.error(err);
+			res.status(500).send("Database error.");
+		} else {
+			var id = req.params.id;
+			var query = module.exports.squel.delete()
+								.from("category")
+								.where("id=?", id);
+			con.query(query.toString(), errorhandler(res, con));
+		}
 	});
 };
 
@@ -72,27 +68,19 @@ module.exports.delete = function deleteCategory(req, res) {
 */
 module.exports.create = function createCategory(req, res) {
 	module.exports.mysql.getConnection(function(err, con) {
-	if (err) {
-		if(con) {
-			con.release();
-		}
-		console.error(err);
-		res.sendStatus(500, "Database error");
-	} else {
-		var name = req.params.name;
-		var query = module.exports.squel.insert().into("category")
-									  	.set("id", null)
-									  	.set("name", name);
-		con.query(query.toString(), function(err, rows) {
-			con.release();
-			if (err) {
-				console.error(err);
-				res.sendStatus(400, "Check the type and structure of your request");
-			} else {
-				res.status(200).json(rows);
+		if (err) {
+			if(con) {
+				con.release();
 			}
-		});
-	}
+			console.error(err);
+			res.status(500).send("Database error.");
+		} else {
+			var name = req.params.name;
+			var query = module.exports.squel.insert().into("category")
+										  	.set("id", null)
+										  	.set("name", name);
+			con.query(query.toString(), errorhandler(res, con));
+		}
 	});
 };
 
@@ -104,28 +92,20 @@ module.exports.create = function createCategory(req, res) {
 */
 module.exports.update = function updateCategory(req, res) {
 	module.exports.mysql.getConnection(function(err, con) {
-	if (err) {
-		if(con) {
-			con.release();
-		}
-		console.error(err);
-		res.sendStatus(500, "Database error");
-	} else {
-		var name = req.params.name;
-		var new_name = req.body.new_name;
-		var query = module.exports.squel.update()
-							.table("category")
-							.set("name", new_name)
-							.where("name=?", name);
-		con.query(query.toString(), function(err, rows) {
-			con.release();
-			if (err) {
-				console.error(err);
-				res.sendStatus(404);
-			} else {
-				res.status(200).json(rows);
+		if (err) {
+			if(con) {
+				con.release();
 			}
-		});
-	}
+			console.error(err);
+			res.status(500).send("Database error.");
+		} else {
+			var name = req.params.name;
+			var new_name = req.body.new_name;
+			var query = module.exports.squel.update()
+								.table("category")
+								.set("name", new_name)
+								.where("name=?", name);
+			con.query(query.toString(), errorhandler(res, con));
+		}
 	});
 };
